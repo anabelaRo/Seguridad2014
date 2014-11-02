df114775b37040ec95965d4d682436402c2decd4
Anabela <anabela.rossi@gmail.com>
2014-11-01 21:24:53 -0300

    Inserts

---
+++ BBDD/inserts.sql
@@ -0,0 +1,134 @@
+/*================================== PRODUCTOS ===============================================*/
+insert into producto values (1, 'TV LED Philips 32 32PHG4109/77', 'Televisores', 'Philips');
+insert into producto values (2, 'TV LED Samsung 40 " UN40F5000 DF', 'Televisores', 'Samsung');
+insert into producto values (3, 'Aire Acondicionado Split Noblex 3220 W NOS32H14X Frío/Calor', 'Aires acondicionados', 'Noblex');
+insert into producto values (4, 'Aire Acondicionado Split Sanyo 3220 W KC1214HSAN Frío/Calor', 'Aires acondicionados', 'Sanyo');
+insert into producto values (5, 'Lavarropas Automático Samsung 7 KG WF1702WEUU Inoxidable', 'Lavarropas', 'Samsung');
+insert into producto values (6, 'Lavarropas Automático Samsung 9 KG WF1904WPUU Inoxidable', 'Lavarropas', 'Samsung');
+insert into producto values (7, 'Heladera con Freezer SIAM HSI-CT10B	$5.699', 'Heladeras', 'SIAM');
+insert into producto values (8, 'Heladera No Frost ATMA HNF420X Acero Inoxidable', 'Heladeras', 'ATMA');
+insert into producto values (9, 'Cámara Digital Sony DSC-HX300/BCE33 Negra', 'Camaras digitales', 'Sony');
+insert into producto values (10, 'Cámara Digital Nikon D5200 Negra', 'Camaras digitales', 'Nikon');
+/*================================== USUARIOS ===============================================*/
+insert into usuario values (1, 'Juan', 'Perez');
+insert into usuario values (2, 'Matías', 'Gonzales');
+insert into usuario values (3, 'Fernando', 'López');
+insert into usuario values (4, 'Agustín', 'Alaimo');
+insert into usuario values (5, 'Esteban', 'Zapata');
+insert into usuario values (6, 'María', 'Meza');
+insert into usuario values (7, 'Noelia', 'Cappria');
+insert into usuario values (8, 'Romina', 'Fernández');
+insert into usuario values (9, 'Juliana', 'Perez');
+insert into usuario values (10, 'Mónica', 'Pereyra');
+/*==================== La primer semana de noviembre tiene precios cargados  ======================*/
+/*========================== por cada usuario para todos los productos ============================*/
+/*================================== PRECIOS prod 1 ===============================================*/
+insert into precio (producto, semana, precio, cargado_por) values (1, '2014-11-01', 4987, 1);
+insert into precio (producto, semana, precio, cargado_por) values (1, '2014-11-02', 6000, 2);
+insert into precio (producto, semana, precio, cargado_por) values (1, '2014-11-04', 5537, 3);
+insert into precio (producto, semana, precio, cargado_por) values (1, '2014-11-04', 4897, 4);
+insert into precio (producto, semana, precio, cargado_por) values (1, '2014-11-05', 5197, 5);
+insert into precio (producto, semana, precio, cargado_por) values (1, '2014-11-06', 6931, 6);
+insert into precio (producto, semana, precio, cargado_por) values (1, '2014-11-07', 7131, 7);
+insert into precio (producto, semana, precio, cargado_por) values (1, '2014-11-02', 7000, 8);
+insert into precio (producto, semana, precio, cargado_por) values (1, '2014-11-01', 6999, 9);
+insert into precio (producto, semana, precio, cargado_por) values (1, '2014-11-05', 5221, 10);
+/*================================== PRECIOS prod 2 ===============================================*/
+insert into precio (producto, semana, precio, cargado_por) values (2, '2014-11-01', 7987, 1);
+insert into precio (producto, semana, precio, cargado_por) values (2, '2014-11-02', 8000, 2);
+insert into precio (producto, semana, precio, cargado_por) values (2, '2014-11-04', 9537, 3);
+insert into precio (producto, semana, precio, cargado_por) values (2, '2014-11-04', 7897, 4);
+insert into precio (producto, semana, precio, cargado_por) values (2, '2014-11-05', 9197, 5);
+insert into precio (producto, semana, precio, cargado_por) values (2, '2014-11-06', 8931, 6);
+insert into precio (producto, semana, precio, cargado_por) values (2, '2014-11-07', 7131, 7);
+insert into precio (producto, semana, precio, cargado_por) values (2, '2014-11-02', 10000, 8);
+insert into precio (producto, semana, precio, cargado_por) values (2, '2014-11-01', 8999, 9);
+insert into precio (producto, semana, precio, cargado_por) values (2, '2014-11-05', 7221, 10);
+/*================================== PRECIOS prod 3 ===============================================*/
+insert into precio (producto, semana, precio, cargado_por) values (3, '2014-11-01', 7987, 1);
+insert into precio (producto, semana, precio, cargado_por) values (3, '2014-11-02', 8000, 2);
+insert into precio (producto, semana, precio, cargado_por) values (3, '2014-11-04', 9537, 3);
+insert into precio (producto, semana, precio, cargado_por) values (3, '2014-11-04', 7897, 4);
+insert into precio (producto, semana, precio, cargado_por) values (3, '2014-11-05', 9197, 5);
+insert into precio (producto, semana, precio, cargado_por) values (3, '2014-11-06', 8931, 6);
+insert into precio (producto, semana, precio, cargado_por) values (3, '2014-11-07', 7131, 7);
+insert into precio (producto, semana, precio, cargado_por) values (3, '2014-11-02', 10000, 8);
+insert into precio (producto, semana, precio, cargado_por) values (3, '2014-11-01', 8999, 9);
+insert into precio (producto, semana, precio, cargado_por) values (3, '2014-11-05', 7221, 10);
+/*================================== PRECIOS prod 4 ===============================================*/
+insert into precio (producto, semana, precio, cargado_por) values (4, '2014-11-01', 7987, 1);
+insert into precio (producto, semana, precio, cargado_por) values (4, '2014-11-02', 8000, 2);
+insert into precio (producto, semana, precio, cargado_por) values (4, '2014-11-04', 9537, 3);
+insert into precio (producto, semana, precio, cargado_por) values (4, '2014-11-04', 7897, 4);
+insert into precio (producto, semana, precio, cargado_por) values (4, '2014-11-05', 9197, 5);
+insert into precio (producto, semana, precio, cargado_por) values (4, '2014-11-06', 8931, 6);
+insert into precio (producto, semana, precio, cargado_por) values (4, '2014-11-07', 7131, 7);
+insert into precio (producto, semana, precio, cargado_por) values (4, '2014-11-02', 10000, 8);
+insert into precio (producto, semana, precio, cargado_por) values (4, '2014-11-01', 8999, 9);
+insert into precio (producto, semana, precio, cargado_por) values (4, '2014-11-05', 7221, 10);
+/*================================== PRECIOS prod 5 ===============================================*/
+insert into precio (producto, semana, precio, cargado_por) values (5, '2014-11-01', 7987, 1);
+insert into precio (producto, semana, precio, cargado_por) values (5, '2014-11-02', 8000, 2);
+insert into precio (producto, semana, precio, cargado_por) values (5, '2014-11-04', 9537, 3);
+insert into precio (producto, semana, precio, cargado_por) values (5, '2014-11-04', 7897, 4);
+insert into precio (producto, semana, precio, cargado_por) values (5, '2014-11-05', 9197, 5);
+insert into precio (producto, semana, precio, cargado_por) values (5, '2014-11-06', 8931, 6);
+insert into precio (producto, semana, precio, cargado_por) values (5, '2014-11-07', 7131, 7);
+insert into precio (producto, semana, precio, cargado_por) values (5, '2014-11-02', 10000, 8);
+insert into precio (producto, semana, precio, cargado_por) values (5, '2014-11-01', 8999, 9);
+insert into precio (producto, semana, precio, cargado_por) values (5, '2014-11-05', 7221, 10);
+/*================================== PRECIOS prod 6 ===============================================*/
+insert into precio (producto, semana, precio, cargado_por) values (6, '2014-11-01', 8987, 1);
+insert into precio (producto, semana, precio, cargado_por) values (6, '2014-11-02', 9000, 2);
+insert into precio (producto, semana, precio, cargado_por) values (6, '2014-11-04', 9537, 3);
+insert into precio (producto, semana, precio, cargado_por) values (6, '2014-11-04', 11897, 4);
+insert into precio (producto, semana, precio, cargado_por) values (6, '2014-11-05', 9197, 5);
+insert into precio (producto, semana, precio, cargado_por) values (6, '2014-11-06', 10931, 6);
+insert into precio (producto, semana, precio, cargado_por) values (6, '2014-11-07', 9131, 7);
+insert into precio (producto, semana, precio, cargado_por) values (6, '2014-11-02', 10000, 8);
+insert into precio (producto, semana, precio, cargado_por) values (6, '2014-11-01', 8999, 9);
+insert into precio (producto, semana, precio, cargado_por) values (6, '2014-11-05', 11221, 10);
+/*================================== PRECIOS prod 7 ===============================================*/
+insert into precio (producto, semana, precio, cargado_por) values (7, '2014-11-01', 5987, 1);
+insert into precio (producto, semana, precio, cargado_por) values (7, '2014-11-02', 6000, 2);
+insert into precio (producto, semana, precio, cargado_por) values (7, '2014-11-04', 7537, 3);
+insert into precio (producto, semana, precio, cargado_por) values (7, '2014-11-04', 6897, 4);
+insert into precio (producto, semana, precio, cargado_por) values (7, '2014-11-05', 6197, 5);
+insert into precio (producto, semana, precio, cargado_por) values (7, '2014-11-06', 7931, 6);
+insert into precio (producto, semana, precio, cargado_por) values (7, '2014-11-07', 6131, 7);
+insert into precio (producto, semana, precio, cargado_por) values (7, '2014-11-02', 7000, 8);
+insert into precio (producto, semana, precio, cargado_por) values (7, '2014-11-01', 5999, 9);
+insert into precio (producto, semana, precio, cargado_por) values (7, '2014-11-05', 7221, 10);
+/*================================== PRECIOS prod 8 ===============================================*/
+insert into precio (producto, semana, precio, cargado_por) values (8, '2014-11-01', 11987, 1);
+insert into precio (producto, semana, precio, cargado_por) values (8, '2014-11-02', 12000, 2);
+insert into precio (producto, semana, precio, cargado_por) values (8, '2014-11-04', 10537, 3);
+insert into precio (producto, semana, precio, cargado_por) values (8, '2014-11-04', 11897, 4);
+insert into precio (producto, semana, precio, cargado_por) values (8, '2014-11-05', 12197, 5);
+insert into precio (producto, semana, precio, cargado_por) values (8, '2014-11-06', 11931, 6);
+insert into precio (producto, semana, precio, cargado_por) values (8, '2014-11-07', 13131, 7);
+insert into precio (producto, semana, precio, cargado_por) values (8, '2014-11-02', 12000, 8);
+insert into precio (producto, semana, precio, cargado_por) values (8, '2014-11-01', 10999, 9);
+insert into precio (producto, semana, precio, cargado_por) values (8, '2014-11-05', 12221, 10);
+/*================================== PRECIOS prod 9 ===============================================*/
+insert into precio (producto, semana, precio, cargado_por) values (9, '2014-11-01', 8987, 1);
+insert into precio (producto, semana, precio, cargado_por) values (9, '2014-11-02', 9000, 2);
+insert into precio (producto, semana, precio, cargado_por) values (9, '2014-11-04', 9537, 3);
+insert into precio (producto, semana, precio, cargado_por) values (9, '2014-11-04', 9897, 4);
+insert into precio (producto, semana, precio, cargado_por) values (9, '2014-11-05', 8197, 5);
+insert into precio (producto, semana, precio, cargado_por) values (9, '2014-11-06', 9931, 6);
+insert into precio (producto, semana, precio, cargado_por) values (9, '2014-11-07', 10031, 7);
+insert into precio (producto, semana, precio, cargado_por) values (9, '2014-11-02', 11000, 8);
+insert into precio (producto, semana, precio, cargado_por) values (9, '2014-11-01', 10999, 9);
+insert into precio (producto, semana, precio, cargado_por) values (9, '2014-11-05', 12221, 10);
+/*================================== PRECIOS prod 10 ===============================================*/
+insert into precio (producto, semana, precio, cargado_por) values (10, '2014-11-01', 15987, 1);
+insert into precio (producto, semana, precio, cargado_por) values (10, '2014-11-02', 16000, 2);
+insert into precio (producto, semana, precio, cargado_por) values (10, '2014-11-04', 15537, 3);
+insert into precio (producto, semana, precio, cargado_por) values (10, '2014-11-04', 14897, 4);
+insert into precio (producto, semana, precio, cargado_por) values (10, '2014-11-05', 16197, 5);
+insert into precio (producto, semana, precio, cargado_por) values (10, '2014-11-06', 15931, 6);
+insert into precio (producto, semana, precio, cargado_por) values (10, '2014-11-07', 16031, 7);
+insert into precio (producto, semana, precio, cargado_por) values (10, '2014-11-02', 15000, 8);
+insert into precio (producto, semana, precio, cargado_por) values (10, '2014-11-01', 15999, 9);
+insert into precio (producto, semana, precio, cargado_por) values (10, '2014-11-05', 16221, 10);
